5a3ff5bbefff76d66a298f8240bafd70c4e806e1
MaTheuz-dev <mathzxdev@gmail.com>
2025-07-01 00:06:00 -0300

    Substituindo código, nova versão

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Login from './pages/Login';
 import Orientacoes from './pages/Orientacoes';
-import Register from './pages/Register';
 import AlertaRiscos from './pages/AlertaRiscos';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/orientacoes" element={<Orientacoes />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/alerta-riscos" element={<AlertaRiscos />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
     
